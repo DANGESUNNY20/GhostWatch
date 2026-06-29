a1d7a48b4cccd4126a8f3cae0e1f93a557376b66
sunny dange <144525768+DANGESUNNY20@users.noreply.github.com>
2026-06-29 06:41:24 +0530

    Add log collection functionality using subprocess

--- collect_logs.py
+++ collect_logs.py
@@ -1,1 +1,20 @@
+import subprocess 
+class logcollect :
+   def collection (self , keyword , log) :
+    result= subprocess.run ([ "grep" ,keyword , log],
+     capture_output=True,
+            text=True)
+return result.stdout
+ 
+  class ctl :
+     def journel (self):
+     result= subprocess.run( 'journelctl -fo |grep -iE "failed|invalid|authentication failure|sudo|error|denied|fatal"'
+      shell=True ,
+      capture_output=True ,
+       text=True
+     )
+        return result.stdout
 
+ 
+
+      
